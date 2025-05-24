3c7e6063b2a97afd7919f5fb62a337d777d443d2
Ayman Chowdhury <aaymanchowdhury@gmail.com>
2025-05-24 16:21:01 +0600

    Initial OS lab topic folders

---
+++ README.md
@@ -0,0 +1,7 @@
+# Operating System Lab Tasks
+This repo contains structured solutions for all OS lab topics:
+- Fork & Process Creation
+- File Management
+- Thread Synchronization
+- CPU Scheduling Algorithms
+- Shell Scripting
---
+++ fork_process/fork_example.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <unistd.h>
+using namespace std;
+
+
+// code for creating 2 child process of a parent process
+
+
+int main() {
+    cout << "Parent PID: " << getpid() << '\n';
+    pid_t pid1 = fork();
+
+    if (pid1 == 0) {
+        cout << "Child 1 PID: " << getpid() << ", Parent PID: " << getppid() << '\n';
+        sleep(10); 
+    } else {
+        pid_t pid2 = fork();
+        if (pid2 == 0) {
+            cout << "Child 2 PID: " << getpid() << ", Parent PID: " << getppid() << '\n';
+            sleep(10); 
+        } else {
+            sleep(10);
+        }
+    }
+    
+
+    return 0;
+}
